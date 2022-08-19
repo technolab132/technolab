b5c3994d960d672b56e83137d53991255298b029
krishna13 <kkwankhede2020@gmail.com>
2022-08-19 18:22:47 +0530

    fifth

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-
+import {Head} from 'next/head';
 import { getCategories, getCategoryPost } from '../../services';
 import { PostCard, Categories, Loader } from '../../components';
 
