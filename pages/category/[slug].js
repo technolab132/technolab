3842cc492bc6430badec231f3ec1d47d765a0d6d
krishna13 <kkwankhede2020@gmail.com>
2022-08-19 18:27:44 +0530

    main coo

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import {Head} from 'next/head';
 import { getCategories, getCategoryPost } from '../../services';
 import { PostCard, Categories, Loader } from '../../components';
 
@@ -12,11 +11,7 @@ const CategoryPost = ({ posts }) => {
   }
 
   return (
-    <>
-    <Head>
-        <title>InnerPage Categories</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  
     <div className="container mx-auto px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
@@ -31,7 +26,6 @@ const CategoryPost = ({ posts }) => {
         </div>
       </div>
     </div>
-    </>
   );
 };
 export default CategoryPost;
